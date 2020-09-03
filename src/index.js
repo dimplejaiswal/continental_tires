9276e76046fb7cacbeb885e8383676113c506569
Dimple Jaiswal <dimplejaiswal@Dimples-MacBook-Pro.local>
2020-09-03 21:00:28 +0700

    UPDATE: This is DMG Project

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
+import ProductList from './containers/ProductList/ProductList';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ProductList />
   </React.StrictMode>,
   document.getElementById('root')
 );
